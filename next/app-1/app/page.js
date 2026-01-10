79324eec86952f07cdc2d5724cb2b61d8f790b55
konno <konno.kenji@gmail.com>
2026-01-10 02:38:53 +0000

    a12

--- next/app-1/app/page.js
+++ next/app-1/app/page.js
@@ -1,8 +1,35 @@
+
 import { useState } from 'react';
 
 function Header({ title }) {
-  return <h1>{title ? title : "Default Title"}</h1>;
+return <h1>{title}</h1>;
 }
 
-export default function HomePage() {
+function HomePage() {
+const names = ['Alice', 'Bob', 'Charlie'];
+
+const [likes, setLikes] = React.useState(0);
+
+function handleClick() {
+    console.log("Incremental log count");
+    setLikes(likes + 1)
 }
+
+
+
+        return (
+            <div>
+                <Header title="React" />
+                <ul>
+                    {names.map((name) => 
+                        <li key={name}>{name}</li>)}
+                </ul>
+                <button onClick={handleClick}>Like  {likes}</button>
+                
+            </div>
+        );
+    }
+
+const lang = ReactDOM.createRoot(app);
+lang.render(<HomePage />);
+
